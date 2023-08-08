1b2500144a225d6667b9cd2a436009f5b03cee7c
Exploit34 <josefexner456@gmail.com>
2023-08-08 00:35:20 -0500

    automatizacion 1

--- script.py
+++ script.py
@@ -1,9 +1,10 @@
 import subprocess
+import sys
 
 def gitCommands():
     try:
-        file = input("Hacer cambio de un archivo S o N: ")
-        if file.lower() == "s": 
+        file = input("Hacer cambio de un solo archivo S o N: ").lower()
+        if file == "s": 
             name_file = input("Nombre del archivo a subir: ")
             subprocess.run(["git", "add", name_file])
             commit_message = input("Introduce el mensaje del commit: ")
@@ -11,16 +12,29 @@ def gitCommands():
             subprocess.run(["git", "push", "-u", "origin", "main"])
             print("¡Los comandos de Git se ejecutaron exitosamente!")
             print(f"Nombre del commit anterior {commit_message}")
-        elif file.lower() == "n":
+        elif file == "n":
             subprocess.run(["git", "add", "."])
             commit_message = input("Introduce el mensaje del commit: ")
             subprocess.run(["git", "commit", "-m", commit_message])
             subprocess.run(["git", "push", "-u", "origin", "main"])
             print("¡Los comandos de Git se ejecutaron exitosamente!")
+            print("----")
             print(f"Nombre del commit anterior {commit_message}")
+        else:
+            print("La opción no es correcta¡")
+            print("-----------------------")
+            gitCommands()
 
     except Exception as e:
         print(f"Ocurrió un error: {e}")
 
 if __name__ == "__main__":
+    print('''
+        ################################
+        #                              #
+        #   <h1>SCRIPT PROCESOS</h1>   #
+        #                              #
+        ################################
+    ''')
     gitCommands()
+    
