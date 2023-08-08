621a8fd7e1bc67404fd41c70f36439d353a7c396
Exploit34 <josefexner456@gmail.com>
2023-08-08 07:56:02 -0500

    pelicula 16

--- js/main.js
+++ js/main.js
@@ -1,46 +1,35 @@
 document.addEventListener("DOMContentLoaded", function() {
     const menuToggler = document.getElementById("menu-toggler");
     const menuDesplegable = document.getElementById("menu-desplegable");
-
+  
     menuToggler.addEventListener("click", function() {
         menuDesplegable.classList.toggle("menu-desplegado");
     });
-});
-
-// Función para obtener el valor del parámetro en la URL por su nombre
-function getParameterByName(name) {
+  
+  });
+  
+  
+  
+  // Función para obtener el valor del parámetro en la URL por su nombre
+  function getParameterByName(name) {
     const url = new URL(window.location.href);
     return url.searchParams.get(name);
-}
-
-
-// Obtenemos todos los botones de "Ver detalles"
-const buttons = document.querySelectorAll(".details-button");
-
-// Agregamos un evento de clic a cada botón
-buttons.forEach((button) => {
+  }
+  
+  // Obtenemos todos los botones de "Ver detalles"
+  const buttons = document.querySelectorAll(".details-button");
+  
+  // Agregamos un evento de clic a cada botón
+  buttons.forEach((button) => {
     button.addEventListener("click", (event) => {
         // Obtenemos el nombre de la película desde el atributo data-pelicula
         const peliculaSeleccionada = event.target.dataset.pelicula;
-
+  
         // Almacenamos el valor en localStorage
         localStorage.setItem("peliculaSeleccionada", peliculaSeleccionada);
-
+  
         // Redirigimos a la página de detalle
         window.location.href = "../views/detalle.html";
     });
-
-peliculas.forEach((pelicula) => {
-        pelicula.addEventListener('mouseenter', (e) => {
-            const elemento = e.currentTarget;
-            setTimeout(() => {
-                peliculas.forEach(pelicula => pelicula.classList.remove('hover'));
-                elemento.classList.add('hover');
-            }, 300);
-        });
-    });
-    
-fila.addEventListener('mouseleave', () => {
-        peliculas.forEach(pelicula => pelicula.classList.remove('hover'));
-    });
-});
+   
+  });
