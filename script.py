523f3e50d18b7acb73e9185622616198b43d6d01
Exploit34 <josefexner456@gmail.com>
2023-08-07 22:37:34 -0500

    automaticacion

--- script.py
+++ script.py
@@ -1,15 +1,26 @@
 import subprocess
 
-def git_commands():
+def gitCommands():
     try:
-        subprocess.run(["git", "add", "."])
-        commit_message = input("Introduce el mensaje del commit: ")
-        subprocess.run(["git", "commit", "-m", commit_message])
-        subprocess.run(["git", "push", "-u", "origin", "main"])
-        print("¡Los comandos de Git se ejecutaron exitosamente!")
-        print("commit anterior"+commit_message)
+        file = input("Hacer cambio de un archivo S o N: ")
+        if file.lower() == "s": 
+            name_file = input("Nombre del archivo a subir: ")
+            subprocess.run(["git", "add", name_file])
+            commit_message = input("Introduce el mensaje del commit: ")
+            subprocess.run(["git", "commit", "-m", commit_message])
+            subprocess.run(["git", "push", "-u", "origin", "main"])
+            print("¡Los comandos de Git se ejecutaron exitosamente!")
+            print(f"Nombre del commit anterior {commit_message}")
+        elif file.lower() == "n":
+            subprocess.run(["git", "add", "."])
+            commit_message = input("Introduce el mensaje del commit: ")
+            subprocess.run(["git", "commit", "-m", commit_message])
+            subprocess.run(["git", "push", "-u", "origin", "main"])
+            print("¡Los comandos de Git se ejecutaron exitosamente!")
+            print(f"Nombre del commit anterior {commit_message}")
+
     except Exception as e:
         print(f"Ocurrió un error: {e}")
 
 if __name__ == "__main__":
-    git_commands()
+    gitCommands()
